e5d2c708ecf58e9359621a6dc7a4bf1d2422f834
Nevena Misic <64745118+nevenamisic@users.noreply.github.com>
2020-12-27 22:53:41 +0100

    Update Stred.c

--- Stred.c
+++ Stred.c
@@ -1,25 +1,27 @@
-#include <linux/kernel.h>//strToInt
+#include <linux/kernel.h>
 #include <linux/device.h>
-#include <linux/string.h>//strlen, strchr
-#include <linux/module.h>// MODULE_LICENSE
-#include <linux/init.h>//module_init, module_exit
-#include <linux/fs.h>//register_chrdev_region, alloc_chrdev_region
-#include <linux/types.h>// dev_t
-#include <linux/cdev.h>//struct cdev,cdevadd
-#include <linux/kdev_t.h>//MAJOR,MINOR, MKDEV
-#include <linux/uaccess.h>//copytouser,copyfromuser
+#include <linux/string.h>
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/fs.h>
+#include <linux/types.h>
+#include <linux/cdev.h>
+#include <linux/kdev_t.h>
+#include <linux/uaccess.h>
 #include <linux/errno.h>
 #define BUFF_SIZE 20
 
 MODULE_LICENSE("Dual BSD/GPL");
+
 dev_t my_dev_id;
 static struct class *my_class;
 static struct device *my_device;
 static struct cdev *my_cdev;
-#define num_of_minors 10
-int storage[num_of_minors];
+
+int storage[10];
 int pos = 0;
 int endRead = 0;
+char string[100];
 
 int storage_open(struct inode *pinode, struct file *pfile);
 int storage_close(struct inode *pinode, struct file *pfile);
@@ -28,68 +30,166 @@ ssize_t storage_write(struct file *pfile, const char __user *buffer, size_t leng
 
 struct file_operations my_fops =
 {
-  .owner = THIS_MODULE,
-  .open = storage_open,
-  .read = storage_read,
-  .write = storage_write,
-  .release = storage_close,
+	.owner = THIS_MODULE,
+	.open = storage_open,
+	.read = storage_read,
+	.write = storage_write,
+	.release = storage_close,
 };
 
 
 int storage_open(struct inode *pinode, struct file *pfile) 
-{   
-  printk(KERN_INFO "Succesfully opened file\n");
-  return 0;
+{
+		printk(KERN_INFO "Succesfully opened file\n");
+		return 0;
 }
 
 int storage_close(struct inode *pinode, struct file *pfile) 
 {
-  printk(KERN_INFO "Succesfully closed file\n");
-  return 0;
+		printk(KERN_INFO "Succesfully closed file\n");
+		return 0;
 }
 
 ssize_t storage_read(struct file *pfile, char __user *buffer, size_t length, loff_t *offset) 
 {
-  int ret;
-  char buff[BUFF_SIZE];
-  long int len;
-  int minor = MINOR(pfile->f_inode->i_rdev);
-  if (endRead){
-    endRead = 0;    
-    printk(KERN_INFO "Succesfully read from file\n");
-    return 0;
-  }
-  len = scnprintf(buff, BUFF_SIZE, "%d\n", storage[minor]);
-  ret = copy_to_user(buffer, buff, len);
-  if(ret)
-    return -EFAULT;
-  endRead = 1;  
-  return len;
+	int ret;
+	char buff[BUFF_SIZE];
+	long int len;
+	if (endRead){
+		endRead = 0;
+		pos = 0;
+		printk(KERN_INFO "Succesfully read from file\n");
+		return 0;
+	}
+	len = scnprintf(buff,BUFF_SIZE , "%d ", storage[pos]);
+	ret = copy_to_user(buffer, buff, len);
+	if(ret)
+		return -EFAULT;
+	pos ++;
+	if (pos == 10) {
+		endRead = 1;
+	}
+	return len;
 }
 
 ssize_t storage_write(struct file *pfile, const char __user *buffer, size_t length, loff_t *offset) 
 {
-  char buff[BUFF_SIZE];
-  int value;
-  int ret;
-  int minor = MINOR(pfile->f_inode->i_rdev);
-  ret = copy_from_user(buff, buffer, length);
-  if(ret)
-    return -EFAULT;
-  buff[length-1] = '\0';
+	char buff[BUFF_SIZE];
+	int position, value;
+	int ret;
+  char string;
+  char clear;
+  char shrink;
+  char append;
+  char truncate;
+  char remove;
+
+	ret = copy_from_user(buff, buffer, length);
+	if(ret)
+		return -EFAULT;
+	buff[length-1] = '\0';
+
+	ret = sscanf(buff,"%d,%d",&value,&position);
+
+	if(ret==2)//two parameters parsed in sscanf
+	{
+		if(position >=0 && position <=9)
+		{
+			storage[position] = value; 
+			printk(KERN_INFO "Succesfully wrote value %d in position %d\n", value, position); 
+		}
+		else
+		{
+			printk(KERN_WARNING "Position should be between 0 and 9\n"); 
+		}
+	}
   
-  ret = sscanf(buff,"%d",&value);
-  storage[minor] = value;
-  printk(KERN_INFO "Succesfully wrote value %d at position %d\n", value, minor);       
-  return length;
+	else
+	{
+		printk(KERN_WARNING "Wrong command format\nexpected: n,m\n\tn-position\n\tm-value\n");
+	}
+
+  string = sscanf(buff,&s,&string);
+  if(string==1)
+  {
+    if(position >=0 && position <=100)
+    {
+      storage[position]=string;
+    }
+    printk(KERN_INFO "&s = Ovo je string koji se obrađuje", string);
+  }
+  else
+	{
+			printk(KERN_WARNING "Position should be between 0 and 9\n"); 
+	}
+	return length;
 }
 
 static int __init storage_init(void)
 {
-  int ret = 0;
-  int i=0;
-  char buff[10];
- 
+   int ret = 0;
+	int i=0;
+
+	//Initialize array
+	for (i=0; i<10; i++)
+		storage[i] = 0;
+
+   ret = alloc_chrdev_region(&my_dev_id, 0, 1, "storage");
+   if (ret){
+      printk(KERN_ERR "failed to register char device\n");
+      return ret;
+   }
+   printk(KERN_INFO "char device region allocated\n");
+
+   my_class = class_create(THIS_MODULE, "storage_class");
+   if (my_class == NULL){
+      printk(KERN_ERR "failed to create class\n");
+      goto fail_0;
+   }
+   printk(KERN_INFO "class created\n");
+   
+   my_device = device_create(my_class, NULL, my_dev_id, NULL, "storage");
+   if (my_device == NULL){
+      printk(KERN_ERR "failed to create device\n");
+      goto fail_1;
+   }
+   printk(KERN_INFO "device created\n");
+
+	my_cdev = cdev_alloc();	
+	my_cdev->ops = &my_fops;
+	my_cdev->owner = THIS_MODULE;
+	ret = cdev_add(my_cdev, my_dev_id, 1);
+	if (ret)
+	{
+      printk(KERN_ERR "failed to add cdev\n");
+		goto fail_2;
+	}
+   printk(KERN_INFO "cdev added\n");
+   printk(KERN_INFO "Hello world\n");
+
+   return 0;
+
+   fail_2:
+      device_destroy(my_class, my_dev_id);
+   fail_1:
+      class_destroy(my_class);
+   fail_0:
+      unregister_chrdev_region(my_dev_id, 1);
+   return -1;
+}
+
+static void __exit storage_exit(void)
+{
+   cdev_del(my_cdev);
+   device_destroy(my_class, my_dev_id);
+   class_destroy(my_class);
+   unregister_chrdev_region(my_dev_id,1);
+   printk(KERN_INFO "Goodbye, cruel world\n");
+}
+
+
+module_init(storage_init);
+module_exit(storage_exit);
   for (i = 0; i < num_of_minors; i++){
       storage[i] = 0;
   }
